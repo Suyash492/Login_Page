a9878d832109d3e731fc9315f1ee8e4da5cf5db3
Suyash492 <suyashbagul492@gmail.com>
2022-01-14 00:24:11 +0530

    second commit

--- login.js
+++ login.js
@@ -1,8 +1,7 @@
-
 function validate()
 {
-    var username=document.getElementById('username').value;
-    var password=document.getElementById('password').value;
+    var username=document.getElementById("Username").value;
+    var password=document.getElementById("password").value;
     if(username== "suyash" && password == "bagul")
     {
         alert("Login succesfully");
